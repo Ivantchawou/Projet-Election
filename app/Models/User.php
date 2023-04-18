d3fd8f5d8e6f79b778ddf8294efb9b95b78b6000
Geekers-Joel237 <79468070+Geekers-Joel237@users.noreply.github.com>
2023-04-18 16:46:58 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,29 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'complete_name',
         'email',
+        'identification_number',
         'password',
+        'age',
+        'citoyennete',
+        'telephone',
+        'residence',
+        'language',
+        'photo',
+        'poste_presente_cdt',
+        'nom_parti_politique_cdt',
+        'exp_politique_cdt',
+        'exp_pro_cdt',
+        'niveau_etude_cdt',
+        'domaine_etude_cdt',
+        'realisations',
+        'reseaux_sociaux',
+        'isConfirm',
+        'isActivated',
+        'role',
+        'pieces_jointes',
+        'email_verified_at'
     ];
 
     /**
@@ -40,5 +60,29 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'reseaux_sociaux' => 'array',
+        'pieces_jointes' => 'json',
+        'isConfirm' => 'boolean',
+        'isActivated' => 'boolean'
     ];
+
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
+
+    public function isCandidat()
+    {
+        return $this->role === 'candidat';
+    }
+
+    public function isOrganisateur()
+    {
+        return $this->role === 'organisateur';
+    }
+
+    public function isElecteur()
+    {
+        return $this->role === 'electeur';
+    }
 }
