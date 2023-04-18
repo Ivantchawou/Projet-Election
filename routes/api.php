21b565dd963dfb68397b2514f93961e2d2b6d133
Geekers-Joel237 <79468070+Geekers-Joel237@users.noreply.github.com>
2023-04-18 17:37:35 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::resource('user', UserController::class);
+
 Route::middleware('auth:sanctum')->group(function () {
     // Routes protégées
-    Route::resource('user', UserController::class);
 });
